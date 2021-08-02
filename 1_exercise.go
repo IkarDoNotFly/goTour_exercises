2e91ffa958b6413d1f278298a8738263c759320a
vyacheslav.mikhalkin <slavikmikhalkin@gmail.com>
2021-08-02 14:47:20 +0300

    try 2

--- 1_exercise.go
+++ 1_exercise.go
@@ -1,1 +1,23 @@
-package goTour_exercises
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//Exercise: Loops and Functions
+
+func Sqrt(x float64) float64 {
+	z:=45.0
+	for ; z*z-x>0.0000001;{
+		z -= (z*z - x) / (2*z)
+		fmt.Println(z)
+	}
+	return z
+}
+
+
+func main() {
+	Sqrt(4)
+	fmt.Println(math.Sqrt(4))
+}
--- 2_exercise.go
+++ 2_exercise.go
@@ -1,1 +1,20 @@
 package main
+//Exercise: Slices
+import (
+	"golang.org/x/tour/pic"
+)
+func Pic(dx, dy int) [][]uint8 {
+
+	sliceDx:=make([]uint8,dx)
+	sliceDy:=make([][]uint8,dy)
+	for i:=range sliceDy{
+		for j:=range sliceDx{
+			sliceDx[j]=uint8(i+j)
+		}
+		sliceDy[i]=sliceDx
+	}
+	return sliceDy
+}
+func main(){
+	pic.Show(Pic)
+}
